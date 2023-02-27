9b60612f9703c029f4f966a3a591c3f246653ba3
Pirasanthan Jesugeevegan <58918065+pirasanthan-jesugeevegan@users.noreply.github.com>
2023-02-27 13:06:25 +0000

    Update build-push.sh
    Typo

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,16 +4,16 @@ set -ex
 source scripts/common.sh
 
 # Build image with current revision, caching from latest
-docker pull -q ${UI_RUNNER_DOCKER_IMAGE_CACHE}
+docker pull -q ${BOT_RUNNER_DOCKER_IMAGE_CACHE}
 docker build \
-  --cache-from ${UI_RUNNER_DOCKER_IMAGE_CACHE} \
-  -t ${UI_RUNNER_DOCKER_IMAGE_REV} \
+  --cache-from ${BOT_RUNNER_DOCKER_IMAGE_CACHE} \
+  -t ${BOT_RUNNER_DOCKER_IMAGE_REV} \
   .
 
-docker push -q ${UI_RUNNER_DOCKER_IMAGE_REV}
+docker push -q ${BOT_RUNNER_DOCKER_IMAGE_REV}
 
 if [ "$GITHUB_BASE_REF" == "master" ]
 then
-    docker tag ${UI_RUNNER_DOCKER_IMAGE_REV} ${UI_RUNNER_DOCKER_IMAGE_CACHE}
-    docker push -q ${UI_RUNNER_DOCKER_IMAGE_CACHE}
+    docker tag ${BOT_RUNNER_DOCKER_IMAGE_REV} ${BOT_RUNNER_DOCKER_IMAGE_CACHE}
+    docker push -q ${BOT_RUNNER_DOCKER_IMAGE_CACHE}
 fi
